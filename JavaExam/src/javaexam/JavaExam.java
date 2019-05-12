eec51f987a2363d3635a409d65bc28f386b18abf
khalid7487 <khalid15-7487@diu.edu.bd>
2019-05-12 12:43:07 +0600

    question

--- JavaExam/src/javaexam/JavaExam.java
+++ JavaExam/src/javaexam/JavaExam.java
@@ -1,26 +1,70 @@
-
 package javaexam;
 
 import java.awt.Dimension;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
-
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class JavaExam {
 
     static QuesFrame fm;
+
     public static void main(String[] args) {
-        fm=new QuesFrame();
+        fm = new QuesFrame();
         fm.setSize(600, 500);
-        Rectangle winDim=fm.getBounds();
-        Dimension screenDim= Toolkit.getDefaultToolkit().getScreenSize();
-        fm.setLocation((int)(screenDim.getWidth()-winDim.width)/2, 
-                (int)(screenDim.getHeight()-winDim.height)/2);
+        Rectangle winDim = fm.getBounds();
+        Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+        fm.setLocation((int) (screenDim.getWidth() - winDim.width) / 2,
+                (int) (screenDim.getHeight() - winDim.height) / 2);
         fm.setIconImage(new ImageIcon("ques.png").getImage());
         fm.setVisible(true);
         fm.setResizable(false);
-        fm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fm.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                final JDialog jd = new JDialog();
+                jd.setModal(true);
+                jd.setTitle("Java Mock Exam Syatem");
+                JLabel msg = new JLabel("Do you want to quit?", JLabel.CENTER);
+                final JButton b1 = new JButton("Ok");
+                final JButton b2 = new JButton("Cencle");
+                b1.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.exit(0);
+                    }
+                });
+                b2.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        jd.hide();
+                          
+                    }
+                });
+                JPanel p = new JPanel();
+                p.add(b1);
+                p.add(b2);
+                Container con = jd.getContentPane();
+                con.add(msg, BorderLayout.NORTH);
+                con.add(p, BorderLayout.SOUTH);
+                jd.setSize(200, 100);
+                Rectangle winDim = jd.getBounds();
+                Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+                jd.setLocation((int) (screenDim.getWidth() - winDim.width) / 2,
+                        (int) (screenDim.getHeight() - winDim.height) / 2);
+                jd.show();
+            }
+
+        });
     }
-    
+
 }
--- JavaExam/src/javaexam/QuesFrame.java
+++ JavaExam/src/javaexam/QuesFrame.java
@@ -1,58 +1,109 @@
-
 package javaexam;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.TextArea;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
+public class QuesFrame extends JFrame {
 
-public class QuesFrame extends JFrame{
-
-    JLabel queNO,timer;
-    int maxQue=1, quesNo=1;
+    JLabel queNO, timer;
+    int maxQue = 1, quesNo = 1;
     TextArea qArea;
-    JCheckBox answer[],mark;
-    JPanel top,center,bottom;
-    JButton next,previous,grade,review;
-    public QuesFrame(){
-        queNO=new JLabel();
-        timer=new JLabel("", JLabel.RIGHT);
-        qArea=new TextArea();
-        answer=new JCheckBox[5];
-        mark=new JCheckBox("Mark");
-        top=new JPanel();
-        center =new JPanel();
-        bottom =new JPanel();
-        center.setFont(new Font("Serieff",Font.PLAIN,12));
+    JCheckBox answer[], mark;
+    JPanel top, center, bottom;
+    JButton next, previous, grade, review;
+    String quesOption[][];
+    boolean ansOption[][];
+
+    public QuesFrame() {
+        quesOption = new String[40][7];
+        ansOption = new boolean[40][6];
+        queNO = new JLabel();
+        timer = new JLabel("", JLabel.RIGHT);
+        qArea = new TextArea();
+        answer = new JCheckBox[5];
+        mark = new JCheckBox("Mark");
+        top = new JPanel();
+        center = new JPanel();
+        bottom = new JPanel();
+        center.setFont(new Font("Serieff", Font.PLAIN, 12));
+        center.setLayout(new GridLayout(5, 1));
         top.setLayout(new BorderLayout());
-        top.add(queNO,BorderLayout.WEST);
-        top.add(mark,BorderLayout.EAST);
+        top.add(queNO, BorderLayout.WEST);
+        top.add(mark, BorderLayout.EAST);
         for (int i = 0; i < 5; i++) {
-            answer[i]=new JCheckBox();
+            answer[i] = new JCheckBox();
             answer[i].addItemListener(null);
             center.add(answer[i]);
         }
         bottom.setLayout(new GridLayout(1, 5));
-        next=new JButton("Next");
-        previous=new JButton("Previous");
-        grade=new JButton("Grade");
-        review=new JButton("Review");
-        previous.setEnabled(true);
+        next = new JButton("Next");
+        previous = new JButton("Previous");
+        grade = new JButton("Grade");
+        review = new JButton("Review");
+        previous.setEnabled(false);
         bottom.add(next);
         bottom.add(previous);
         bottom.add(grade);
         bottom.add(review);
-        getContentPane().add(top,BorderLayout.NORTH);
-        getContentPane().add(center,BorderLayout.CENTER);
-        getContentPane().add(bottom,BorderLayout.SOUTH);
+        getContentPane().add(top, BorderLayout.NORTH);
+        getContentPane().add(bottom, BorderLayout.SOUTH);
+        getContentPane().add(center, BorderLayout.CENTER);
         next.addActionListener(null);
         previous.addActionListener(null);
         grade.addActionListener(null);
         review.addActionListener(null);
-        
+
         setTitle("Java Mock Exam Syatem");
     }
-    
+}
+
+class Question {
+
+    RandomAccessFile file;
+    String quesOption[][] = new String[40][7];
+
+    public Question() {
+
+        try {
+            file = new RandomAccessFile("Question.txt", "r");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Question.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    String[][] getQuestion() {
+        StringBuffer s1;
+        try {
+            for (int i = 0; i < 40; i++) {
+                StringBuffer sb1 = new StringBuffer();
+                b1:
+                while (true) {
+                    String temp = new String(file.readLine());
+                    if (temp.equals("##-------Question----------##")) {
+                        int k = (int) Math.ceil(Math.random() * 2);
+                        switch (k) {
+                            case 2:
+                                break b1;
+                        }
+                    }
+                }
+                while (true) {
+                    s1=new StringBuffer(file.readLine());
+                    if (new String(s1).equals("##-------Question----------##"))
+                    break;
+                }else if(){
+                               
+                }
+            }
+        } catch (Exception e) {
+        }
+        return quesOption;
+    }
 }
